3400b557177014199ecc5a6897a4e7f983ffbb12
Max Carlquist <tenkaklet@gmail.com>
2019-01-07 14:20:49 +0100

    updated to es6

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var form = document.querySelector('#form');
-var content = document.querySelector('#content');
+const form = document.querySelector('#form');
+const content = document.querySelector('#content');
 
-var age = prompt('How old are you?');
+const age = prompt('How old are you?');
 
 if (age > 18) {
     // show website
     content.style.display = 'block';
-    var name = prompt('What is your name?');
+    let name = prompt('What is your name?');
     document.querySelector('.message').innerHTML = "Welcome " + name + " to something really cool!";
     document.querySelector('#enter').className = 'granted';
 } else {
